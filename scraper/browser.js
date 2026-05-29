1d3769d4a5c0b50fb031912719823d1bb93fe372
vrnaik17-collab <vrnaik17@gmail.com>
2026-05-29 12:47:30 +0530

    Update browser.js

--- scraper/browser.js
+++ scraper/browser.js
@@ -41,7 +41,9 @@ async function createContext(browserInstance) {
       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
       'Accept-Encoding': 'gzip, deflate, br',
       'Connection': 'keep-alive',
-      'Upgrade-Insecure-Requests': '1'
+      'Upgrade-Insecure-Requests': '1',
+      // Referer makes it look like navigation came from the idbf.in homepage
+      'Referer': 'https://idbf.in/'
     }
   });
 
